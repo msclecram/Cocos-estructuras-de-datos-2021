3f572cd9fb81564277f8dea18a46b9badba9655c
Ivan Santiago Gutierrez Estrada <55468122+TheTerribleS@users.noreply.github.com>
2021-04-20 20:02:02 -0500

    OG base project copy

---
+++ IvanSantiago/VehicleProjectBase/Source/EDDProject/EDDProjectWheelFront.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VehicleWheel.h"
+#include "EDDProjectWheelFront.generated.h"
+
+UCLASS()
+class UEDDProjectWheelFront : public UVehicleWheel
+{
+	GENERATED_BODY()
+
+public:
+	UEDDProjectWheelFront();
+};
+
+
+
