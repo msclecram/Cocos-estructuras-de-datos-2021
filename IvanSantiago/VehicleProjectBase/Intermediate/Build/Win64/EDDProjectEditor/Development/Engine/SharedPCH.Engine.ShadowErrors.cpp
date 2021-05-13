017b49af1c1535c22c025e46dc24e4b4f7ce64f3
Ivan Santiago Gutierrez Estrada <55468122+TheTerribleS@users.noreply.github.com>
2021-05-13 18:46:36 -0500

    Revert "Revert "Revert "enhacements 120521"""
    This reverts commit d640f3d42fbbdfb874be53f4098e54257c4ec685.

--- IvanSantiago/VehicleProjectBase/Intermediate/Build/Win64/EDDProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29914
-#include "C:/Users/Ivan/Escuela/EstructuraDeDatos/Cocos-estructuras-de-datos-2021/IvanSantiago/VehicleProjectBase/Intermediate/Build/Win64/EDDProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
