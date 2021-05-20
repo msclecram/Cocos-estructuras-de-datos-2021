1861ad16c657856bc56a4cda245be99fbe49a666
VicPina <vicpanzu7@gmail.com>
2021-05-20 12:24:46 -0500

    Project upload

---
+++ Vic Piña/Proyecto UE4/ProyectoUnreal/VehicleProjectBase/Source/EDDProject/EDDProjectWheelRear.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EDDProjectWheelRear.h"
+
+UEDDProjectWheelRear::UEDDProjectWheelRear()
+{
+	ShapeRadius = 35.f;
+	ShapeWidth = 10.0f;
+	bAffectedByHandbrake = true;
+	SteerAngle = 0.f;
+}
